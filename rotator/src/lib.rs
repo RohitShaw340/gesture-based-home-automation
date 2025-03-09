841f5e717c48e855a731e418dccf3c44167c9a4b
Paul <1mayanktomar@gmail.com>
2025-03-09 15:53:46 +0530

    init

---
+++ rotator/src/lib.rs
@@ -0,0 +1,75 @@
+use std::time::Duration;
+
+use rppal::gpio::Gpio;
+
+pub struct Servo {
+    pin: rppal::gpio::OutputPin,
+    pub period_ms: u64,
+    pub pulse_min_us: u64,
+    pub pulse_neutral_us: u64,
+    pub pulse_max_us: u64,
+    pub hw_pwm: bool,
+    pub min_angle: f64,
+    pub max_angle: f64,
+}
+
+impl Servo {
+    pub fn new(
+        gpio_pwm: u8,
+        period_ms: u64,
+        pulse_min_us: u64,
+        pulse_neutral_us: u64,
+        pulse_max_us: u64,
+        min_angle: f64,
+        max_angle: f64,
+        hw_pwm: bool,
+    ) -> Result<Self, rppal::gpio::Error> {
+        let gpio = Gpio::new()?;
+        let mut pin = gpio.get(gpio_pwm)?.into_output();
+        Ok(Self {
+            pin,
+            period_ms,
+            pulse_min_us,
+            pulse_neutral_us,
+            pulse_max_us,
+            hw_pwm,
+            min_angle,
+            max_angle,
+        })
+    }
+
+    pub fn rotate(&self, angle: f64) -> Result<(), rppal::gpio::Error> {
+        if self.hw_pwm {
+            self.rotate_hwpwm(angle)
+        } else {
+            self.rotate_sfpwm(angle)
+        }
+    }
+
+    fn rotate_sfpwm(&self, angle: f64) -> Result<(), rppal::gpio::Error> {
+        self.pin.set_pwm(
+            Duration::from_millis(self.period_ms),
+            Duration::from_micros(self.angle_to_pulse_width(angle)),
+        )?;
+
+        Ok(())
+    }
+
+    fn rotate_hwpwm(&self, angle: f64) -> Result<(), rppal::gpio::Error> {
+        todo!()
+    }
+
+    fn angle_to_pulse_width(&self, angle: f64) -> u64 {
+        // TODO: handle error
+        assert!(self.min_angle >= angle);
+        assert!(self.max_angle <= angle);
+        let mid = (self.max_angle - self.min_angle) / 2;
+        if angle < mid {
+            self.pulse_min_us
+                + (angle - self.min_angle) * (self.pulse_neutral_us - self.pulse_min_us)
+        } else {
+            self.pulse_max_us
+                - (self.pulse_max_us - angle) * (self.pulse_max_us - self.pulse_neutral_us)
+        }
+    }
+}
---
+++ rotator/src/main.rs
@@ -0,0 +1,66 @@
+use std::error::Error;
+
+use clap::Parser;
+use rotator::Servo;
+
+// Period: 20 ms (50 Hz). Pulse width: min. 600 µs, neutral 1500 µs, max. 2250 µs.
+const PERIOD_MS: u64 = 20;
+const PULSE_MIN_US: u64 = 600;
+const PULSE_NEUTRAL_US: u64 = 1500;
+const PULSE_MAX_US: u64 = 2250;
+
+const MIN_ANGLE: f64 = -80;
+const MAX_ANGLE: f64 = 80;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    angle: f64,
+    #[arg(short, long)]
+    pin: u8,
+    #[arg(long, default_value_t = MIN_ANGLE)]
+    min_angle: f64,
+    #[arg(long, default_value_t = MAX_ANGLE)]
+    max_angle: f64,
+    #[arg(long, default_value_t = PERIOD_MS)]
+    period_ms: u64,
+    #[arg(long, default_value_t = PULSE_MAX_US)]
+    pulse_min_us: u8,
+    #[arg(long, default_value_t = PULSE_NEUTRAL_US)]
+    pulse_neutral_us: u8,
+    #[arg(long, default_value_t = PULSE_MAX_US)]
+    pulse_max_us: u8,
+    #[arg(long, default_value_t = false)]
+    hw_pwm: bool,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let Args {
+        angle,
+        pin,
+        min_angle,
+        max_angle,
+        period_ms,
+        pulse_min_us,
+        pulse_neutral_us,
+        pulse_max_us,
+        hw_pwm,
+    } = Args::parse();
+
+    let servo = Servo::new(
+        pin,
+        period_ms,
+        pulse_min_us,
+        pulse_neutral_us,
+        pulse_max_us,
+        min_angle,
+        max_angle,
+        hw_pwm,
+    )?;
+
+    servo.rotate(angle)?;
+
+    Ok(())
+}
